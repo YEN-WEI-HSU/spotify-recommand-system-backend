b77a0d5559b49c39e3f5b429df01588f5312de65
yumi0507 <michellechang958@gmail.com>
2025-06-01 18:43:33 +0000

    database

--- backend/settings.py
+++ backend/settings.py
@@ -85,8 +85,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'spotify',
+        'USER': 'postgres',
+        'PASSWORD': 'user123',        
+        'HOST': 'localhost',
+        'PORT': '26713',
     }
 }
 
